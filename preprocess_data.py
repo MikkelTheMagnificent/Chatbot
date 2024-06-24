267ca4e19d097b0453cd083322d7b47adc9b0732
Scrumlord123 <mikkel@cargo-bi.com>
2024-06-24 13:05:47 +0200

    Update

--- preprocess_data.py
+++ preprocess_data.py
@@ -4,12 +4,14 @@ import numpy as np
 import random
 import nltk
 from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import train_test_split
 
 nltk.download('punkt')
 nltk.download('wordnet')
 
 lemmatizer = WordNetLemmatizer()
 
+# Load intents json file
 with open('intents.json') as file:
     intents = json.load(file)
 
@@ -18,29 +20,34 @@ classes = []
 documents = []
 ignore_letters = ['?', '!', '.', ',']
 
+# Iterate through each sentence in intents patterns
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        word_list = nltk.word_tokenize(pattern)
+        word_list = nltk.word_tokenize(pattern)  # Tokenization
         words.extend(word_list)
         documents.append((word_list, intent['tag']))
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
+# Lemmatization and lowercasing
 words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in ignore_letters]
 words = sorted(set(words))
 
 classes = sorted(set(classes))
 
+# Save words and classes
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
 
 training = []
 output_empty = [0] * len(classes)
 
+# Create the traning data
 for document in documents:
     bag = []
     word_patterns = document[0]
     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    # Feature Extraction: numerize words
     for word in words:
         bag.append(1) if word in word_patterns else bag.append(0)
 
@@ -53,7 +60,10 @@ random.shuffle(training)
 train_x = np.array([np.array(t[0]) for t in training])
 train_y = np.array([np.array(t[1]) for t in training])
 
+# Split data into training and test sets //////////
+train_x, test_x, train_y, test_y = train_test_split(train_x, train_y, test_size=0.2, random_state=42)
+
 with open('train_data.pkl', 'wb') as file:
-    pickle.dump((train_x, train_y), file)
+    pickle.dump((train_x, train_y, test_x, test_y), file)
 
 print("Preprocessing complete.")
--- train_model.py
+++ train_model.py
@@ -3,25 +3,46 @@ import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import SGD
+from sklearn.metrics import classification_report
 
-# Load preprocessed data
+# Load preprocessed data including test data
 with open('train_data.pkl', 'rb') as file:
-    train_x, train_y = pickle.load(file)
+    train_x, train_y, test_x, test_y = pickle.load(file)
+
+# Load classes
+with open('classes.pkl', 'rb') as file:
+    classes = pickle.load(file)
 
 # Building the model
-model = Sequential() 
-model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu')) 
-model.add(Dropout(0.5)) 
-model.add(Dense(64, activation='relu')) 
-model.add(Dropout(0.5)) 
-model.add(Dense(len(train_y[0]), activation='softmax')) 
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu')) # First layer
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu')) # Second layer
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax')) # Output layer
 
 # Compiling the model
 sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy']) # Hyperparameter tuning
 
-# Training the model
-hist = model.fit(train_x, train_y, epochs=1000, batch_size=5, verbose=1)
+# Training the model with validation data
+hist = model.fit(train_x, train_y, epochs=1000, batch_size=5, verbose=1, validation_data=(test_x, test_y))
 model.save('chatbot_model.h5', hist)
 
-print("Training complete.")
+# Evaluate the model on test data
+test_loss, test_accuracy = model.evaluate(test_x, test_y, verbose=0)
+print(f"Test Accuracy: {test_accuracy}")
+
+# Predict classes on test data
+y_pred = model.predict(test_x)
+y_pred_classes = np.argmax(y_pred, axis=1)
+y_true = np.argmax(test_y, axis=1)
+
+
+unique_classes = np.unique(y_true)
+
+# Generate a classification report with the correct labels
+report = classification_report(y_true, y_pred_classes, labels=unique_classes, target_names=[classes[i] for i in unique_classes])
+print(report)
+
+print("Training complete and performance metrics calculated.")
